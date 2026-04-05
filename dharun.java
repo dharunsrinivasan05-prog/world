98812efc68b3b013bf83639b5a26877d1799316b
dharunsrinivasan05-prog <dharunsrinivasan05@gmail.com>
2026-04-05 20:40:51 +0530

    Add files via upload

---
+++ dharun.java
@@ -0,0 +1,15 @@
+package hidharun;
+public class dharun {
+
+    public static void main(String[] args) {
+        
+        // Check if command-line argument is provided
+        if (args.length > 0) {
+            String name = args[0];
+            System.out.println("Hello, " + name + "!");
+        } else {
+            // Default message if no argument is passed
+            System.out.println("Hello, World!");
+        }
+    }
+}
