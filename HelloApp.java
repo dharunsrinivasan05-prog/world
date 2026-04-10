f0d0559511b4d03aea703e6f71e72077f7f9426f
dharunsrinivasan05-prog <dharunsrinivasan05@gmail.com>
2026-04-10 08:36:18 +0530

    Add files via upload

---
+++ HelloApp.java
@@ -0,0 +1,11 @@
+package hidharun;
+public class HelloApp {
+
+    // Main method - Entry point of the program
+    public static void main(String[] args) {
+
+        // Print "Hello World" to the console
+        System.out.println("Hello World");
+
+    }
+}
